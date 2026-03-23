176328d795d07cfa2d8ebf796cf858d7c6ae809c
Ricky109 <114699211+Ricky109@users.noreply.github.com>
2026-03-22 21:32:33 -0500

    faltó una coma xd

--- server/db.js
+++ server/db.js
@@ -10,7 +10,7 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false // Requerido para conexiones seguras en Render
-  }
+  },
   max: 2,
   connectionTimeoutMillis: 5000,
 });
